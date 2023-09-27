b75e2f7facc8b28d7b6a0de9185112034e46fcde
Aditya Alshi <alshiaditya55@gmail.com>
2023-09-27 05:59:01 +0530

    nav logo update

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Nav() {
   return (
     <div className="nav-wrapper">
-      <img className="nav-logo" src="public\images\travel-icon.png" alt="" />
+      <img className="nav-logo" src="public\travel-icon.png" alt="" />
       <h3 className="nav-journal-heading">My Travel Journal</h3>
     </div>
   );
