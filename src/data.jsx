7c57529b20338819ab0f805381054ae9e32aea9c
Aditya Alshi <alshiaditya55@gmail.com>
2023-09-27 07:57:10 +0530

    images path updates

--- src/data.jsx
+++ src/data.jsx
@@ -7,7 +7,7 @@ export default [
         startDate: "January 26, 2023",
         endDate: "January 30, 2023",
         description: "Richly decorated caves dating from 2nd & 1st centuries BC, home to masterpieces of religious art.",
-        imageUrl: "public/ajanta.jpg",
+        imageUrl: "ajanta.jpg",
     },
     {
         id: 1,
@@ -17,7 +17,7 @@ export default [
         startDate: "January 10, 2019",
         endDate: "January 14, 2019",
         description: "The Maldives, officially the Republic of Maldives, is an archipelagic state and country in South Asia, situated in the Indian Ocean. It lies southwest of Sri Lanka and India, about 750 kilometres from the Asian continent's mainland.",
-        imageUrl: "public/maldivs.jpg",
+        imageUrl: "maldivs.jpg",
     },
     {
         id: 2,
@@ -27,6 +27,6 @@ export default [
         startDate: "August 8, 2014",
         endDate: "July 29, 2019",
         description: "Hilltop place of worship with an unusual tower combining architecture styles of many religions.",
-        imageUrl: "public/ambulawa.jpg",
+        imageUrl: "ambulawa.jpg",
     }
 ]
